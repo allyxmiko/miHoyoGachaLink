cf13dcaa4ad46e1b1a326eec64eddfe322e9cec8
allyxmiko <allyxmiko@163.com>
2023-09-27 22:18:21 +0800

    feat: 完成v1.0.0版本

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"miHoyoGachaLink/constant"
 	"miHoyoGachaLink/server"
 )
 
 func main() {
+	fmt.Println("当前版本：v" + constant.Version)
 	fmt.Println("1. 点击链接前请先登录游戏查看一下对应游戏的抽卡记录。")
 	fmt.Println("2. 查看记录后请确保游戏关闭。")
 	fmt.Println("3. 点击以下对应链接获取对应的抽卡链接。")
